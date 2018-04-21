3539f7ed3591bd207edaa37df698fc8b62f63183
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-04-21 11:04:58 +0700

    - Refactor code and done.

--- src/components/UserControl.js
+++ src/components/UserControl.js
@@ -31,37 +31,53 @@ class UserControl extends Component {
 
   render() {
 
-    var { username, password } = this.state;
-
     return (
       <div className="row bg-faded p-4 my-4">
         <div className="box">
           <div className="col-lg-12">
-            <hr />
-            <h2 className="intro-text text-center">Login <strong>form</strong>
-            </h2>
-            <hr />
-            <form role="form" onSubmit={ this.handleSubmit }>
-              <div className="row">
-                <div className="form-group col-lg-10">
-                  <label>UserName</label>
-                  <input name='username' type="text" className="form-control" value={ username } onChange={ this.handleChange } />
-                </div>
-                <div className="form-group col-lg-10">
-                  <label>Password</label>
-                  <input name='password' type="password" className="form-control" value={ password } onChange={ this.handleChange } />
-                </div>
-                <div className="clearfix" />
-                <div className="form-group col-lg-12">
-                    <input type="hidden" name="save" defaultValue="contact" />
-                    <button type="submit" className="btn btn-default">Submit</button>
-                </div>
-              </div>
-            </form>
+            { this.getFormLogin() }
           </div>
         </div>
       </div>
     );
+  }
+
+  getFormLogin = () => {
+    var { username, password } = this.state;
+    var { user } = this.props;
+
+    var xhtml = <h2>You are logged!</h2>;
+    if(!user.isLogin) {
+      xhtml = <div>
+                <hr />
+                <h2 className="intro-text text-center">Login <strong>form</strong></h2>
+                <hr />
+                <form role="form" onSubmit={ this.handleSubmit }>
+                  <div className="row">
+                    <div className="form-group col-lg-10">
+                      <label>UserName</label>
+                      <input name='username' type="text" className="form-control" value={ username } onChange={ this.handleChange } />
+                    </div>
+                    <div className="form-group col-lg-10">
+                      <label>Password</label>
+                      <input name='password' type="password" className="form-control" value={ password } onChange={ this.handleChange } />
+                    </div>
+                    <div className="clearfix" />
+                    <div className="form-group col-lg-12">
+                        <input type="hidden" name="save" defaultValue="contact" />
+                        <button type="submit" className="btn btn-default">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+    }
+    return xhtml;
+  }
+}
+var mapStateToProps = (state) => {
+  var { user } = state;
+  return {
+      user
   }
 }
 
@@ -74,4 +90,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(UserControl);
+export default connect(mapStateToProps, mapDispatchToProps)(UserControl);
